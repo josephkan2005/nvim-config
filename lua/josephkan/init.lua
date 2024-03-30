480d6b3cee6428d3948f5ed0be9f26ecd3f87493
Person1234565 <josephkan14@gmail.com>
2024-03-29 23:46:47 -0700

    finished moving to modules

--- lua/josephkan/init.lua
+++ lua/josephkan/init.lua
@@ -1,3 +1,17 @@
 require 'josephkan.set'
 require 'josephkan.remap'
 require 'josephkan.lazy_init'
+
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
