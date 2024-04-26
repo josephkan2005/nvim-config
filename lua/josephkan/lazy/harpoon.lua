7d662f34c7c7784a25233c2b41dd8152878e8d04
Person1234565 <josephkan14@gmail.com>
2024-04-25 20:38:02 -0700

    update harpoon keybinds

--- lua/josephkan/lazy/harpoon.lua
+++ lua/josephkan/lazy/harpoon.lua
@@ -37,18 +37,37 @@ return {
       harpoon.ui:toggle_quick_menu(harpoon:list())
     end, { desc = 'Open Harpoon window' })
 
-    vim.keymap.set('n', '<leader>hl', function()
+    vim.keymap.set('n', '<leader>h;', function()
       harpoon:list():select(1)
     end, { desc = 'Select first buffer' })
-    vim.keymap.set('n', '<leader>hk', function()
+
+    vim.keymap.set('n', '<leader>hl', function()
       harpoon:list():select(2)
     end, { desc = 'Select second buffer' })
-    vim.keymap.set('n', '<leader>hj', function()
+
+    vim.keymap.set('n', '<leader>hk', function()
       harpoon:list():select(3)
     end, { desc = 'Select third buffer' })
-    vim.keymap.set('n', '<leader>hh', function()
+
+    vim.keymap.set('n', '<leader>hj', function()
       harpoon:list():select(4)
     end, { desc = 'Select fourth buffer' })
+
+    vim.keymap.set('n', '<leader>ho', function()
+      harpoon:list():select(5)
+    end, { desc = 'Select fifth buffer' })
+
+    vim.keymap.set('n', '<leader>hi', function()
+      harpoon:list():select(6)
+    end, { desc = 'Select sixth buffer' })
+
+    vim.keymap.set('n', '<leader>hu', function()
+      harpoon:list():select(7)
+    end, { desc = 'Select seventh buffer' })
+
+    vim.keymap.set('n', '<leader>hh', function()
+      harpoon:list():select(8)
+    end, { desc = 'Select eighth buffer' })
 
     -- Toggle previous & next buffers stored within Harpoon list
     vim.keymap.set('n', '<leader>hp', function()
