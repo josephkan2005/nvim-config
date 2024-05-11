44c1d4c4a0d7f5b6b486441e5ab44b45635a25e3
Person1234565 <josephkan14@gmail.com>
2024-05-11 15:20:29 -0700

    gdb run options

--- lua/josephkan/lazy/dap.lua
+++ lua/josephkan/lazy/dap.lua
@@ -81,20 +81,55 @@ return {
   config = function()
     local dap = require 'dap'
     dap.adapters.gdb = {
+      id = 'gdb',
       type = 'executable',
       command = 'gdb',
-      args = { '-i', 'dap' },
+      args = { '--quiet', '--interpreter=dap' },
     }
     dap.configurations.c = {
       {
-        name = 'Launch',
+        name = 'Run executable (GDB)',
         type = 'gdb',
         request = 'launch',
+        -- This requires special handling of 'run_last', see
+        -- https://github.com/mfussenegger/nvim-dap/issues/1025#issuecomment-1695852355
         program = function()
-          return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+          local path = vim.fn.input {
+            prompt = 'Path to executable: ',
+            default = vim.fn.getcwd() .. '/',
+            completion = 'file',
+          }
+
+          return (path and path ~= '') and path or dap.ABORT
         end,
-        cwd = '${workspaceFolder}',
-        stopAtBeginningOfMainSubprogram = false,
+      },
+      {
+        name = 'Run executable with arguments (GDB)',
+        type = 'gdb',
+        request = 'launch',
+        -- This requires special handling of 'run_last', see
+        -- https://github.com/mfussenegger/nvim-dap/issues/1025#issuecomment-1695852355
+        program = function()
+          local path = vim.fn.input {
+            prompt = 'Path to executable: ',
+            default = vim.fn.getcwd() .. '/',
+            completion = 'file',
+          }
+
+          return (path and path ~= '') and path or dap.ABORT
+        end,
+        args = function()
+          local args_str = vim.fn.input {
+            prompt = 'Arguments: ',
+          }
+          return vim.split(args_str, ' +')
+        end,
+      },
+      {
+        name = 'Attach to process (GDB)',
+        type = 'gdb',
+        request = 'attach',
+        processId = require('dap.utils').pick_process,
       },
     }
     dap.configurations.cpp = dap.configurations.c
