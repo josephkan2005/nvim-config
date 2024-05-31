6f72c855b2eb36c0d6d2259dec8ee7bb23a163c3
Person1234565 <josephkan14@gmail.com>
2024-05-30 19:37:08 -0700

    prettier for json

--- lua/josephkan/lazy/conform.lua
+++ lua/josephkan/lazy/conform.lua
@@ -14,6 +14,7 @@ return { -- Autoformat
       tsx = { 'prettierd' },
       jsx = { 'prettierd' },
       javascript = { 'prettierd' },
+      json = { 'prettierd' },
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
       --
