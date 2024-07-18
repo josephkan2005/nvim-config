e76d0c24da20a4b49c10c0ce43a7b97d9ef8e80a
Person1234565 <josephkan14@gmail.com>
2024-07-17 22:01:39 -0700

    add netrw remap desc

--- lua/josephkan/remap.lua
+++ lua/josephkan/remap.lua
@@ -5,7 +5,7 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 --vim.keymap.set('i', 'jk', '<Esc>')
 
-vim.keymap.set('n', '<leader>ev', vim.cmd.Ex)
+vim.keymap.set('n', '<leader>ev', vim.cmd.Ex, { desc = 'Open Netrw' })
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 
