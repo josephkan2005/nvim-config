06c2fd217f776e41ed0a40edb417b226e15366c3
Person1234565 <josephkan14@gmail.com>
2024-07-27 01:23:30 -0700

    add trouble which key

--- lua/josephkan/lazy/which-key.lua
+++ lua/josephkan/lazy/which-key.lua
@@ -11,6 +11,7 @@ return { -- Useful plugin to show you pending keybinds.
       ['<leader>h'] = { name = '[H]arpoon', _ = 'which_key_ignore' },
       ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
       ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+      ['<leader>t'] = { name = '[T]rouble', _ = 'which_key_ignore' },
       ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
     }
   end,
