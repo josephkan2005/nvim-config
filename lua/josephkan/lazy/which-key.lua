f7ace4313338e23d18d7fa503926cb382bf1542f
Person1234565 <josephkan14@gmail.com>
2024-08-03 00:54:09 -0700

    update modules

--- lua/josephkan/lazy/which-key.lua
+++ lua/josephkan/lazy/which-key.lua
@@ -5,14 +5,14 @@ return { -- Useful plugin to show you pending keybinds.
     require('which-key').setup()
 
     -- Document existing key chains
-    require('which-key').register {
-      ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-      ['<leader>d'] = { name = '[D]ebugger', _ = 'which_key_ignore' },
-      ['<leader>h'] = { name = '[H]arpoon', _ = 'which_key_ignore' },
-      ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-      ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-      ['<leader>t'] = { name = '[T]rouble', _ = 'which_key_ignore' },
-      ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+    require('which-key').add {
+      { '<leader>c', group = '[C]ode' },
+      { '<leader>d', group = '[D]ebugger' },
+      { '<leader>h', group = '[H]arpoon' },
+      { '<leader>r', group = '[R]ename' },
+      { '<leader>s', group = '[S]earch' },
+      { '<leader>t', group = '[T]rouble' },
+      { '<leader>w', group = '[W]orkspace' },
     }
   end,
 }
