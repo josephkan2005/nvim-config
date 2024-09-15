b9ded23dc5195e589be76bae65c0668ff82646c8
Person1234565 <josephkan14@gmail.com>
2024-09-15 13:54:29 -0700

    revert zathura simple

--- lua/josephkan/lazy/vimtex.lua
+++ lua/josephkan/lazy/vimtex.lua
@@ -2,7 +2,7 @@ return {
   'lervag/vimtex',
   lazy = false,
   init = function()
-    vim.g.vimtex_view_method = 'zathura_simple'
+    vim.g.vimtex_view_method = 'zathura'
     vim.g.vimtex_quickfix_enabled = 0
   end,
 }
