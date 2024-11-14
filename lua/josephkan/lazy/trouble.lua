4ab3d47da1ef0ddcf18ec7f854c08c37f3668653
Person1234565 <josephkan14@gmail.com>
2024-11-14 15:52:37 -0800

    filter todos

--- lua/josephkan/lazy/trouble.lua
+++ lua/josephkan/lazy/trouble.lua
@@ -1,9 +1,44 @@
+local tagIdx = {}
+local tags = {
+  [0] = '',
+  [1] = 'FIX',
+  [2] = 'TODO',
+  [3] = 'WARN',
+  [4] = 'NOTE',
+}
+local numTags = 0
+for key, value in pairs(tags) do
+  tagIdx[value] = key
+  numTags = numTags + 1
+end
+
 return {
   'folke/trouble.nvim',
   opts = {
     open_no_results = true,
     win = {
       size = 0.4,
+    },
+    ---@type table<string, trouble.Mode>
+    modes = {
+      todo = {
+        mode = 'todo',
+        keys = {
+          s = {
+            action = function(view)
+              print(view.get()['mode'])
+              local f = view:get_filter 'tag'
+              local tag = tags[((tagIdx[f and f.filter.tag] or 0) + 1) % numTags]
+              view:filter({ tag = tag }, {
+                id = 'tag',
+                template = '{hl:Title}Filter:{hl} {tag}',
+                del = tag == '',
+              })
+            end,
+            desc = 'Toggle Tag Filter',
+          },
+        },
+      },
     },
   },
   cmd = 'Trouble',
@@ -15,7 +50,7 @@ return {
     },
     {
       '<leader>to',
-      '<cmd>Trouble todo toggle focus=true filter = {tag = {TODO,FIX,FIXME}}<cr>',
+      '<cmd>Trouble todo toggle focus=true<cr>',
       desc = 'Todo (Trouble)',
     },
     {
