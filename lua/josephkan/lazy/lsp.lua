b9c2b8bd413b7884bd326ba766f2bdc67c860c79
josephkan2005 <josephkan14@gmail.com>
2025-10-16 21:00:55 -0700

    enable keymap set mods

--- lua/josephkan/lazy/lsp.lua
+++ lua/josephkan/lazy/lsp.lua
@@ -55,8 +55,8 @@ return { -- LSP Configuration & Plugins
         --
         -- In this case, we create a function that lets us more easily define mappings specific
         -- for LSP related items. It sets the mode, buffer and description for us each time.
-        local map = function(keys, func, desc)
-          vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+        local map = function(keys, func, desc, modes)
+          vim.keymap.set(modes or 'n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
         end
 
         -- Jump to the definition of the word under your cursor.
@@ -90,7 +90,7 @@ return { -- LSP Configuration & Plugins
 
         -- Execute a code action, usually your cursor needs to be on top of an error
         -- or a suggestion from your LSP for this to activate.
-        map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+        map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'v' })
 
         -- Opens a popup that displays documentation about the word under your cursor
         --  See `:help K` for why this keymap
