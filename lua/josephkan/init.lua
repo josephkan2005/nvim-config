dcb0c7402a8907c1bc6c52a979844712faeeb612
josephkan2005 <josephkan14@gmail.com>
2025-10-20 16:09:11 -0700

    rename

--- lua/josephkan/init.lua
+++ lua/josephkan/init.lua
@@ -10,7 +10,7 @@ require 'josephkan.lazy_init'
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  group = vim.api.nvim_create_augroup('config-highlight-yank', { clear = true }),
   callback = function()
     vim.highlight.on_yank()
   end,
