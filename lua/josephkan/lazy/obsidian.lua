d5734b4746b28bc6b53cbccfd08f23771d92b424
josephkan2005 <josephkan14@gmail.com>
2025-12-15 20:31:25 -0800

    load obsidian if in vault

--- lua/josephkan/lazy/obsidian.lua
+++ lua/josephkan/lazy/obsidian.lua
@@ -1,17 +1,31 @@
+local workspaces = {
+  {
+    name = 'personal',
+    path = '~/vaults/personal',
+  },
+}
+
 return {
   'obsidian-nvim/obsidian.nvim',
   version = '*', -- recommended, use latest release instead of latest commit
-  ft = 'markdown',
+  -- ft = 'markdown',
+  cond = function()
+    for _, workspace in ipairs(workspaces) do
+      local vault_path = vim.fn.expand(workspace.path)
+      local cwd = vim.fn.getcwd()
+
+      if vim.startswith(cwd, vault_path) then
+        return true
+      end
+    end
+    return false
+  end,
+
   ---@module 'obsidian'
   ---@type obsidian.config
   opts = {
     legacy_commands = false,
     ui = { ignore_conceal_warn = true },
-    workspaces = {
-      {
-        name = 'personal',
-        path = '~/vaults/personal',
-      },
-    },
+    workspaces = workspaces,
   },
 }
