faad7d1c61e5c15f272c8302dac788259c89ce03
josephkan2005 <josephkan14@gmail.com>
2026-08-14 12:36:41 -0700

    remove some keybinds

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -3,19 +3,7 @@ vim.g.maplocalleader = ' '
 
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
---vim.keymap.set('i', 'jk', '<Esc>')
-
 vim.keymap.set('n', '<leader>ev', vim.cmd.Explore, { desc = 'Open Netrw' })
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
-
-vim.keymap.set('n', '<leader>p', '"0p')
-vim.keymap.set('n', '<leader>P', '"0P')
-
-vim.keymap.set('n', '<C-Up>', '<C-w>-')
-vim.keymap.set('n', '<C-Down>', '<C-w>+')
-vim.keymap.set('n', '<C-Left>', '<C-w><')
-vim.keymap.set('n', '<C-Right>', '<C-w>>')
 
 vim.keymap.set('n', '<C-w>d', function()
   vim.diagnostic.open_float { border = 'solid' }
