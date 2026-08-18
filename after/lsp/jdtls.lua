29d7bb6a776b608840083dc3c3019b495ac924bd
josephkan2005 <josephkan14@gmail.com>
2026-08-18 13:02:17 -0700

    add sources to jdtls

--- after/lsp/jdtls.lua
+++ after/lsp/jdtls.lua
@@ -1,7 +1,22 @@
 return {
   settings = {
     java = {
-      configuration = {},
+      configuration = {
+        runtimes = {
+          {
+            name = 'JavaSE-25',
+            path = '/usr/lib/jvm/java-25-openjdk-amd64',
+            sources = '/usr/lib/jvm/java-25-openjdk-amd64/lib/src.zip',
+            default = true,
+          },
+        },
+      },
+      eclipse = {
+        downloadSources = true,
+      },
+      maven = {
+        downloadSources = true,
+      },
       symbols = {
         includeSourceMethodDeclarations = true,
       },
